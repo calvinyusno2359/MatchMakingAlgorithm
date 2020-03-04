43285fa9f6032f139103a29d2f3781b2245b2d70
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-03-04 21:52:25 +0800

    redirects to main page after end chat is successful

--- views/assets/scripts/chat.js
+++ views/assets/scripts/chat.js
@@ -9,16 +9,16 @@ function pushText(text, side, sending=false) {
         }
         let message = document.createElement("p");
         message.textContent = text;
-    
+
         bubble.appendChild(message);
         container.appendChild(bubble);
         chat.appendChild(container);
-    
+
         chat.scrollTop = chat.scrollHeight;
-    
+
         return bubble;
     }
-    
+
     function sendMessage() {
         if (content.textContent != "") {
             receipt_queue.push(pushText(content.textContent, "right", true));
@@ -26,7 +26,7 @@ function pushText(text, side, sending=false) {
         }
         content.textContent = "";
     }
-    
+
     function keyPress(e) {
         if (e.key == "Enter") {
             sendMessage();
@@ -37,15 +37,16 @@ function pushText(text, side, sending=false) {
             send.disabled = true;
         }
     }
-    
+
     // end chat and close conversation
     function endChat() {
         const id = {
-            userId: user_id 
+            userId: user_id
         }
         disconnect('/chat/disconnect', id).then(() => {
             closeConversation().then(() => {
                 console.log("Conversation closed successfully")
+                window.location.pathname = '/'
             }).catch(() => {
                 console.log("Conversation closed unsuccessfully ")
             })
@@ -54,19 +55,19 @@ function pushText(text, side, sending=false) {
 
     async function disconnect(url = '', data = {}) {
         const response = await fetch(url, {
-          method: 'POST', 
+          method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(data) 
+          body: JSON.stringify(data)
         });
-        return await response.json(); 
+        return await response.json();
       }
 
     const closeConversation = async() => {
         await rainbowSDK.conversations.closeConversation(conversation)
     }
-    
+
     async function waitConnection() {
         // ping server for token and id
         let response = await fetch("/chat/request");
@@ -75,24 +76,24 @@ function pushText(text, side, sending=false) {
         let agent_id = result.agent_id;
         user_id = result.user_id;
         response = await rainbowSDK.connection.signinSandBoxWithToken(token);
-    
+
         // get agent, add to network, and open conversation
         let contact = await rainbowSDK.contacts.searchById(agent_id);
         conversation = await rainbowSDK.conversations.openConversationForContact(contact);
         await rainbowSDK.im.getMessagesFromConversation(conversation);
-    
+
         document.addEventListener(rainbowSDK.im.RAINBOW_ONNEWIMMESSAGERECEIVED, receive);
         document.addEventListener(rainbowSDK.im.RAINBOW_ONNEWIMRECEIPTRECEIVED, receipt);
-    
+
         document.body.appendChild(chat);
         document.body.appendChild(content);
         document.body.appendChild(send);
         document.body.appendChild(end); 
-    
+
         send.addEventListener("click", sendMessage);
         content.addEventListener("keyup", keyPress);
         end.addEventListener("click", endChat);
-    
+
         conversation.messages.forEach(message => {
             if (message.side === "R") {
                 pushText(message.data, "right");
@@ -101,19 +102,19 @@ function pushText(text, side, sending=false) {
             }
         });
     }
-    
+
     function receive(e) {
         let message = e.detail.message;
         pushText(message.data, "left");
     }
-    
+
     function receipt(e) {
         if (e.detail.evt !== "received") {
             return;
         }
-    
+
         let message = e.detail.message.data;
-    
+
         for(let i=0; i<receipt_queue.length; i++) {
             if (receipt_queue[i].querySelector("p").textContent == message) {
                 receipt_queue[i].className = "right";
@@ -122,20 +123,20 @@ function pushText(text, side, sending=false) {
             }
         }
     }
-    
+
     function onLoaded() {
         rainbowSDK.setVerboseLog(false);
         rainbowSDK.initialize();
     }
-    
+
     // window.onbeforeunload = async function() {
     //     if (account_id == "") return;
-    
+
     //     // unregister visitor from db
     //     let init = {"method": "POST", "body": {"account_id": account_id}};
     //     await fetch("https://esc.xuliang.dev/api/chat/done", init);
     // }
-    
+
     let conversation;
     let user_id = "";
     let receipt_queue = [];
@@ -153,12 +154,11 @@ function pushText(text, side, sending=false) {
     end.className = "end_button";
     end.textContent = "End Chat"
     send.disabled = true;
-    
+
     angular.bootstrap(document, ["sdk"]).get("rainbowSDK");
-    
+
     document.addEventListener(rainbowSDK.RAINBOW_ONLOADED, onLoaded);
     document.addEventListener(rainbowSDK.RAINBOW_ONREADY, waitConnection);
-    
+
     rainbowSDK.load();
-    
-    
+
