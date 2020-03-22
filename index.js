763284dcdacdb4c80f7e822ab20f53822e7b3bb0
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-03-22 23:32:53 +0800

    changed for heroku deployment

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/admin', admin.populateAgents);
 app.post('/admin/addagent', admin.addAgent);
 app.post('/admin/updateagent', admin.updateAgent);
 app.get('/admin/deleteagent/:id', admin.deleteAgent);
-app.get('/admin/getagents/:id', admin.getAgents); 
+app.get('/admin/getagents/:id', admin.getAgents);
 app.get('/admin/updateagentavail/:id', admin.updateAgentAvailability);
 
 // starts rainbowsdk
@@ -52,9 +52,9 @@ rainbowSDK.start();
 let PORT = process.env.PORT || 8080
 
 // for localhost deployment: use self-issued ssh found in config.js
-https.createServer({ key: config.key, cert: config.cert }, app).listen(PORT, () => {
-    console.log(`App listening on port ${PORT}! Go to https://localhost:${PORT}/`);
-});
+// https.createServer({ key: config.key, cert: config.cert }, app).listen(PORT, () => {
+//     console.log(`App listening on port ${PORT}! Go to https://localhost:${PORT}/`);
+// });
 
 // for heroku deployment: ssl certificate for https is managed by heroku's Auto Cert Management
 app.listen(PORT, () => console.log(`Listening to port: ${PORT}...`));
