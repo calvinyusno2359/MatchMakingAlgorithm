7bebd3fb4a5e7c6ec5b8c9a258f6a0e0bc866821
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-03-23 21:26:58 +0800

    finish converting queue junit test to mocha-chai framework

--- handlers/queue.js
+++ handlers/queue.js
@@ -1,5 +1,5 @@
 function Queue() {
-    console.log("Queue Initiated.");
+    // console.log("Queue Initiated.");
 
     this.q = [];
     this.verbosity = false;
--- tests/mochaQueueTest.js
+++ tests/mochaQueueTest.js
@@ -2,16 +2,81 @@
 const assert = require('chai').assert;
 const queue = require("../handlers/queue");
 
-describe('Queue Test: Methods', () => {
-  it("Queue is inititalized properly.", () => {
-    assert.isTrue(true);
+describe('Queue Test: Attributes', () => {
+  let q;
+
+  beforeEach((done) => {
+    q = new queue.Queue();
+    done();
   });
 
+  it("Queue must have empty array", () => {
+    assert.isTrue(Array.isArray(q.q), "Queue does not have array");
+    assert.equal(q.q.length, 0, "Queue array length is not 0");
+  });
+});
+
+describe('Queue Test: Methods', () => {
+  let q;
+
+  beforeEach((done) => {
+    q = new queue.Queue();
+    q.enqueue("1");
+    q.enqueue("2");
+    q.enqueue("3");
+    q.enqueue("4");
+    done();
+  });
+
+  it("Queue enqueue() works properly", () => {
+    let expected = ["1", "2", "3", "4"];
+    for (var i=0; i<expected.length; i++) {
+      assert.equal(q.q[i], expected[i], "Queue enqueue() does not return [1,2,3,4]");
+    };
+  });
+
+  it("Queue dequeue() works properly", () => {
+    let top = q.dequeue();
+    assert.equal(top, "1", "Queue dequeue() does return 1")
+
+    let expected = ["2", "3", "4"];
+    for (var i=0; i<expected.length; i++) {
+      assert.equal(q.q[i], expected[i], "Queue dequeue() subsequent return is incorrect");
+    };
+
+    q.dequeue();
+    q.dequeue();
+    assert.equal(q.q[0], "4", "Queue dequeue() last element is not 4");
+  });
+
+  it("Queue length() works properly", () => {
+    assert.equal(q.length(), 4, "Queue length() does return 4");
+  });
+
+  it("Queue peek() works properly", () => {
+    assert.equal(q.peek(), "1", "Queue length() does return 1");
+    q.dequeue();
+    q.dequeue();
+    assert.equal(q.peek(), "3", "Queue length() does return 3");
+  })
 });
 
 describe('Queue Test: Scenarios', () => {
-  it("Queue is inititalized properly.", () => {
-    assert.isTrue(true);
+  beforeEach((done) => {
+    q = new queue.Queue();
+    done();
+  });
+
+  it("dequeue() empty queue", () => {
+    assert.equal(q.q.length, 0, "Queue array length is not 0");
+    let isNull = q.dequeue();
+    assert.isNull(isNull, null, "Queue array length is not null");
+  });
+
+  it("peek() empty queue", () => {
+    assert.equal(q.q.length, 0, "Queue array length is not 0");
+    let isUndefined = q.peek();
+    assert.isUndefined(isUndefined, "Queue array length is not undefined");
   });
 
 });
