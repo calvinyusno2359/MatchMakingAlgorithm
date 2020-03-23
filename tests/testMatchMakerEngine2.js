6836a1634472079a2436315f8b69d4381d77618e
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-03-23 14:28:55 +0800

    finish generateMatch

--- tests/testMatchMakerEngine2.js
+++ tests/testMatchMakerEngine2.js
@@ -9,5 +9,19 @@ async function test_getAllAvailableAgent() {
   matchmaker = await matchmaker.getAllAvailableAgent();
 };
 
-console.log("\ntest_getAllAvailableAgent")
-test_getAllAvailableAgent();
+async function test_generateMatch() {
+  matchmaker = await matchmaker.getAllAvailableAgent();
+  matchmaker = await matchmaker.generateMatch("Back");
+};
+
+async function test_matchUser() {
+  matchmaker = await matchmaker.getAllAvailableAgent();
+  matchmaker = await matchmaker.matchUser("user1", "Back");
+};
+
+// console.log("\ntest_getAllAvailableAgent")
+// test_getAllAvailableAgent();
+console.log("\ntest_generateMatch")
+test_generateMatch();
+console.log("\ntest_matchUser");
+test_matchUser();
