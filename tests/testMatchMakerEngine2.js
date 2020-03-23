1268e79c27d8b56e7b8084b28a3a599c5c84e89e
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-03-23 15:25:50 +0800

    finish overhauilng matchUser

--- tests/testMatchMakerEngine2.js
+++ tests/testMatchMakerEngine2.js
@@ -16,12 +16,36 @@ async function test_generateMatch() {
 
 async function test_matchUser() {
   matchmaker = await matchmaker.getAllAvailableAgent();
-  matchmaker = await matchmaker.matchUser("user1", "Back");
+
+  agentId = await matchmaker.matchUser("user1", "Back");
+  console.log(agentId)
+};
+
+async function test_matchUser_multiple() {
+  matchmaker = await matchmaker.getAllAvailableAgent();
+
+  agentId = await matchmaker.matchUser("user1", "Back");
+  console.log("signal1:", agentId) // first agent
+
+  agentId = await matchmaker.matchUser("user1", "Back");
+  console.log("signal1:", agentId) // first agent (reconnect case)
+
+  agentId = await matchmaker.matchUser("user2", "Back");
+  console.log("signal2:", agentId) // second agent
+
+  agentId = await matchmaker.matchUser("user3", "Back");
+  console.log("signal3:", agentId) // WAIT (queued, tell wait)
+
+  agentId = await matchmaker.matchUser("user3", "Back");
+  console.log("signal3:", agentId) // WAIT (still queueing!)
+
 };
 
 // console.log("\ntest_getAllAvailableAgent")
 // test_getAllAvailableAgent();
-console.log("\ntest_generateMatch")
-test_generateMatch();
-console.log("\ntest_matchUser");
-test_matchUser();
+// console.log("\ntest_generateMatch")
+// test_generateMatch();
+// console.log("\ntest_matchUser");
+// test_matchUser();
+console.log("\ntest_matchUser_multiple");
+test_matchUser_multiple();
