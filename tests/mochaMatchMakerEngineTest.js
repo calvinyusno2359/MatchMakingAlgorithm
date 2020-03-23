12ba2a6238a60ecbf696b20867b620da0869c578
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-03-23 20:22:01 +0800

    reformat tests, setup structure

---
+++ tests/mochaMatchMakerEngineTest.js
@@ -0,0 +1,17 @@
+// unit test for Queue object
+const assert = require('chai').assert;
+const queue = require("../handlers/queue");
+
+describe('MatchMakerEngine Test 1', () => {
+  it("blah b", () => {
+    assert.isTrue(true);
+  });
+
+});
+
+describe('MatchMakerEngine Test 2', () => {
+  it("blah b", () => {
+    assert.isTrue(true);
+  });
+
+});
---
+++ tests/mochaQueueTest.js
@@ -0,0 +1,10 @@
+// unit test for Queue object
+const assert = require('chai').assert;
+const queue = require("../handlers/queue");
+
+describe('Queue Test 1', () => {
+  it("blah a", () => {
+    assert.isTrue(true);
+  });
+
+});
