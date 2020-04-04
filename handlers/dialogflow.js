bb757e43f2dfcca17f31029e42b019d05def646d
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-04-04 15:27:21 +0800

    minor typo

--- handlers/dialogflow.js
+++ handlers/dialogflow.js
@@ -1,9 +1,11 @@
 function fulfill(req, res) {
 	if (req.body.queryResult.action === 'queue') {
 		req.body.queryResult.fulfillmentMessages = "from server"
+		res.send(req.body.queryResult);
+	}
+	else {
 		res.send(req.body);
 	}
-	res.send(req.body.queryResult);
 }
 
 // exports
