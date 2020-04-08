7ac3fb600d0b0c98b4c2996e409a8d7bebe19482
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-04-09 01:50:59 +0800

    swap to local deployment

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ https.createServer({ key: config.key, cert: config.cert }, app).listen(PORT, () 
 });
 
 // for heroku deployment: ssl certificate for https is managed by heroku's Auto Cert Management
-//app.listen(PORT, () => console.log(`Listening to port: ${PORT}...`));
+// app.listen(PORT, () => console.log(`Listening to port: ${PORT}...`));
 
