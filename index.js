e54ca11d8fe4a258b96543ce330608b36c3512d0
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-04-12 22:04:27 +0800

    swapped to local deployment

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ rainbowSDK.start();
 let PORT = process.env.PORT || 8080
 
 // for localhost deployment: use self-issued ssh found in config.js
-// https.createServer({ key: config.key, cert: config.cert }, app).listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}! Go to https://localhost:${PORT}/`);
-// });
+https.createServer({ key: config.key, cert: config.cert }, app).listen(PORT, () => {
+    console.log(`App listening on port ${PORT}! Go to https://localhost:${PORT}/`);
+});
 
 // for heroku deployment: ssl certificate for https is managed by heroku's Auto Cert Management
-app.listen(PORT, () => console.log(`Listening to port: ${PORT}...`));
+// app.listen(PORT, () => console.log(`Listening to port: ${PORT}...`));
 
